5a56ef41cbf301055cb970c3f67c8f0223ff46f9
Anon-136 <anonjin44@gmail.com>
2022-12-15 12:03:42 +0700

    Remove vue motion

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import { MotionPlugin } from "@vueuse/motion";
-
 import "./style.css";
 
 const app = createApp(App);
-
-app.use(MotionPlugin);
 
 app.mount("#app");
